316426b8260c9c06be78fe449d1797eaf43f20c9
Eric Busch <busch@jymmin.com>
2022-07-29 21:52:40 +0200

    split main into two functions
    file reading and csv parsing

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,41 @@ mod client;
 mod client_transaction_handler;
 mod transaction;
 
+use crate::client_transaction_handler::ClientTransactionHandler;
+use app_state::AppState;
 use client::Client;
 use transaction::Transaction;
 
-use app_state::AppState;
+fn read_csv_from_file(file_path: &str) -> Result<String, std::io::Error> {
+    let mut file = std::fs::File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
 
-use crate::client_transaction_handler::ClientTransactionHandler;
+fn parse_transactions(csv_data: String) -> Result<(), csv::Error> {
+    let mut reader = csv::Reader::from_reader(csv_data.as_bytes());
+
+    let mut app_state = AppState::new();
+    for transaction in reader.deserialize() {
+        app_state
+            .add_transaction(transaction?)
+            .expect("Something went wrong while adding the transaction");
+    }
+
+    // quick hack for client csv output
+    println!("client,available,held,total,locked");
+    for client in app_state.clients().values() {
+        println!("{}", client.as_csv());
+    }
+    Ok(())
+}
 
 fn main() -> Result<(), csv::Error> {
     let file_path = std::env::args()
         .nth(1)
         .unwrap_or_else(|| "data.csv".to_string());
-    let mut app_state = AppState::new();
-    let mut file = std::fs::File::open(file_path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    let mut reader = csv::Reader::from_reader(contents.as_bytes());
-
-    for transaction in reader.deserialize() {
-        let transaction: Transaction = transaction?;
-        // println!("{}", &transaction);
-        // check if transaction client exists, create client if not
-        app_state
-            .add_transaction(transaction)
-            .expect("Something went wrong while adding the transaction");
-    }
-    println!("client,available,held,total,locked");
-    for client in app_state.clients().values() {
-        println!("{}", client.as_csv());
-    }
-
+    let csv_data = read_csv_from_file(&file_path)?;
+    parse_transactions(csv_data)?;
     Ok(())
 }
